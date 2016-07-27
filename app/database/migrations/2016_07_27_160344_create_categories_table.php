c2a9cfa56fdb23491c91959efdcbe2209aeb507c
Sirlam <salam_lanre@yahoo.com>
2016-07-27 17:08:07 +0100

    Created migrations and controller for the categories table

---
+++ app/database/migrations/2016_07_27_160344_create_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//Creating the categories table
+        Schema::create('categories', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//Drop the categories table
+        Schema::drop('categories');
+	}
+
+}
