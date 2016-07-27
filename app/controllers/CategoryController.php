0ec7899ca06d606334d0b041b3c9db17c42e18ce
Sirlam <salam_lanre@yahoo.com>
2016-07-27 18:06:05 +0100

    Created models for product and category

--- app/controllers/CategoryController.php
+++ app/controllers/CategoryController.php
@@ -1,12 +1,2 @@
 <?php
 
-class CategoryController extends Eloquent
-{
-    protected $fillable = array('name');
-
-    protected static $rules = array('array'=>'required|min3');
-
-    public function Products(){
-        return $this->hasMany('Product');
-    }
-}
---
+++ app/models/Category.php
@@ -0,0 +1,13 @@
+<?php
+
+
+class Category extends Eloquent
+{
+    protected $fillable = array('name');
+
+    protected static $rules = array('array'=>'required|min3');
+
+    public function Products(){
+        return $this->hasMany('Product');
+    }
+}
